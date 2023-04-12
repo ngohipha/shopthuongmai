b5c14bb4708f586b8d313f5acab0f2ee6e4e9e94
ngohipha <ngohipha@gmail.com>
2023-04-12 12:31:36 +0700

    product , "page", "sort", "limit", "fields"

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ const authRouter = require("./routes/authRoute");
 const bodyParser = require("body-parser");
 const { notFound, errorHandler } = require("./middlewares/errorHandler");
 const cookieParser = require('cookie-parser')
+const productRouter = require('./routes/productRoute')
+const morgan = require('morgan')
 dbConnect();
 
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use("/api/user", authRouter);
-
+app.use("/api/product",productRouter)
 app.use(notFound);
 app.use(errorHandler);
 
